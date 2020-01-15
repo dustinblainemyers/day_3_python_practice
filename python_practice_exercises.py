45c86c92e792f63fe449cedbe6ff6cce4dadc36f
Dustin Myers <dustinblaine.myers@gmail.com>
2020-01-15 11:47:25 -0500

    added the magic number excercise file we did in class.  Also started on python practice exercises we were working on before lunch .  For the practice exercises , I created a function that allows you to choose which exercise you want to run , and then runs that exercises code based on a series of if/ else statements

---
+++ python_practice_exercises.py
@@ -0,0 +1,35 @@
+
+# first problem in python practice
+
+choice = input("What exercise do you want to run ? ")
+
+
+
+def todays_exercises(choice):
+    
+    if choice == "1":
+        print("This is exercise 1 ")
+        name = input('What is your name?: ')
+        print('Hello, %s!' % name)
+    elif choice == "2":
+        print("This is exercise 2 ")
+        
+
+
+
+
+
+
+todays_exercises(choice)
+
+
+    
+
+    
+
+
+
+
+    
+
+    
