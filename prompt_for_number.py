3d260f2d48131946e6c3582af12b7c2111ffabea
Dustin Myers <dustinblaine.myers@gmail.com>
2020-01-15 10:21:01 -0500

    python exercise prompt user for a number , multiply by itself and return value to user.

---
+++ prompt_for_number.py
@@ -0,0 +1,7 @@
+# prompt a user for a number , multiply that number by itself.  Then return value to user.
+
+user_number = int(input("Please enter a number: "))
+result = str(user_number * user_number)
+print(" Your number multiplied by itself is: " + result)
+
+
