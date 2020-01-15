2835ae5b699d547674e5cb452e2f44026f8abb14
Dustin Myers <dustinblaine.myers@gmail.com>
2020-01-15 12:02:50 -0500

    completed the python exercises assigned before lunch.  Checked and they are running as expected

--- python_practice_exercises.py
+++ python_practice_exercises.py
@@ -13,9 +13,19 @@ def todays_exercises(choice):
         print('Hello, %s!' % name)
     elif choice == "2":
         print("This is exercise 2 ")
-        
+        name = (input('What is your name?: ')).upper()
+    
+        print('Hello, %s!' % name)
+        letters_in_name = len(name)
+        print("Your name has %d letters in it! Awesome ! " % letters_in_name)
 
-
+    elif choice == "3":
+        print("Please fill in the blanks below: ")
+        print("____(name)____'s favorite subject in school is ____(subject)____.")
+        name = input("What is your name? ")
+        subject = input("What is your favorite subject in school? ")
+        # outputs the persons name and favorite subject
+        print("%s's favorite subject in school is %s." % (name, subject))
 
 
 
